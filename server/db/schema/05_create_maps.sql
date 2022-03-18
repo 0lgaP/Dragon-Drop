f2613866dd96c3f844d8455f5bb5dacaa02ace80
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-18 11:30:02 -0400

    add schema for maps

---
+++ server/db/schema/05_create_maps.sql
@@ -0,0 +1,9 @@
+-- schema/05_create_maps.sql
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+DROP TABLE IF EXISTS maps CASCADE;
+-- CREATE USERS
+CREATE TABLE maps (
+  id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+  name VARCHAR(100),
+  campaign_id FOREIGN KEY REFERENCES campaigns(id)
+);
