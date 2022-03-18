ca53f887429753f4b695c59ff251991b536950c8
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-18 16:19:16 -0400

    add seeds for map assets

--- server/db/seeds/03_images.sql
+++ server/db/seeds/03_images.sql
@@ -5,6 +5,6 @@
 
 INSERT INTO images (id, name, src) VALUES
 ('a29a1114-5687-4732-bae6-31e45baa0978','The Cage', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZjIhQYJAKltl9QjqqSQ4vjk9goWnXTt1zjNaTF4gYzdoGTYOj'),
-('e6f69b18-81cb-4c89-8739-c10ee48a6b92','The Cage', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZjIhQYJAKltl9QjqqSQ4vjk9goWnXTt1zjNaTF4gYzdoGTYOj'),
-('bb4c3683-7bd2-496c-8a0a-6b7e78e37c1d','The Cage', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZjIhQYJAKltl9QjqqSQ4vjk9goWnXTt1zjNaTF4gYzdoGTYOj'),
-('fb483912-ae59-4082-9c21-d144443072dc','The Cage ', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZjIhQYJAKltl9QjqqSQ4vjk9goWnXTt1zjNaTF4gYzdoGTYOj');
+('e6f69b18-81cb-4c89-8739-c10ee48a6b92','The Cage2', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZjIhQYJAKltl9QjqqSQ4vjk9goWnXTt1zjNaTF4gYzdoGTYOj'),
+('bb4c3683-7bd2-496c-8a0a-6b7e78e37c1d','The Cage3', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZjIhQYJAKltl9QjqqSQ4vjk9goWnXTt1zjNaTF4gYzdoGTYOj'),
+('fb483912-ae59-4082-9c21-d144443072dc','The Cage4', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZjIhQYJAKltl9QjqqSQ4vjk9goWnXTt1zjNaTF4gYzdoGTYOj');
--- server/db/seeds/08_asset_types.sql
+++ server/db/seeds/08_asset_types.sql
@@ -7,8 +7,8 @@
 
 INSERT INTO asset_types (id, name) VALUES
 ('2eb2339a-0adf-4cc4-bc09-6e27d43f15b4', 'NPC'),
-('fa8dbb44-f356-45ae-9b57-4c07c95c56f0', 'PLAYER'),
-('ced32085-6daf-464f-b341-daf3624999cf', 'IMAGE');
+('ced32085-6daf-464f-b341-daf3624999cf', 'IMAGE'),
+('fa8dbb44-f356-45ae-9b57-4c07c95c56f0', 'STORY_CARD');
 -- ('c11c94ca-e8ef-4539-9b4c-35a4092ba526',),
 -- ('cc620ff0-a677-457b-a625-f52f39347bca',),
 -- ('53366308-cb8a-4c69-bdef-8efd546b0d5d',);
