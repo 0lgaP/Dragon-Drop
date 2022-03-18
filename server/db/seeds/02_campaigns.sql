0a87b0f3d999438103803430a4647dc41a3eaa07
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-18 13:56:50 -0230

    change serial primary keys to uuid

--- server/db/seeds/02_campaigns.sql
+++ server/db/seeds/02_campaigns.sql
@@ -1,6 +1,6 @@
 -- seeds/02_campaigns.sql
 -- campaigns seeds
-INSERT INTO campaigns (id, name, dm_id) VALUES (1, 'Middle Earth', 1);
-INSERT INTO campaigns (id, name, dm_id) VALUES (2, 'Elsewyr', 1);
-INSERT INTO campaigns (id, name, dm_id) VALUES (3, 'Johto', 2);
-INSERT INTO campaigns (id, name, dm_id) VALUES (4, 'Shiganshina', 1);
+INSERT INTO campaigns (id, name, dm_id) VALUES ('8a89386b-de43-4c63-9127-3a78394d4253', 'Middle Earth', '2c41cf56-a6d7-11ec-b909-0242ac120002');
+INSERT INTO campaigns (id, name, dm_id) VALUES ('407a2fd0-f57e-4110-bf73-8f11917ecab9', 'Elsewyr', '2c41cf56-a6d7-11ec-b909-0242ac120002');
+INSERT INTO campaigns (id, name, dm_id) VALUES ('b819024a-4fd2-4316-8697-411ad293bb71', 'Johto', '4896e484-a6d7-11ec-b909-0242ac120002');
+INSERT INTO campaigns (id, name, dm_id) VALUES ('06673f43-2b6e-4fd6-812c-0b86d778d5a9', 'Shiganshina', '2c41cf56-a6d7-11ec-b909-0242ac120002');
--- server/db/seeds/04_npcs.sql
+++ server/db/seeds/04_npcs.sql
@@ -1,19 +1,19 @@
 -- seeds/04_npcs.sql
 -- NPCs seeds
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
-  VALUES (1, null, 1, 'Merla Brushgather', true, 'A Halfling farmer looking for her lost goat, Morty.', 'Town1');
+  VALUES ('2c9267a4-2dbf-4ce4-91b3-ae6a514fa795', null, '8a89386b-de43-4c63-9127-3a78394d4253', 'Merla Brushgather', true, 'A Halfling farmer looking for her lost goat, Morty.', 'Town1');
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
-  VALUES (2, null, 1, 'Morty', true, 'A goat belonging to Merla Brushgather.', 'Town1');
+  VALUES ('c6b15ff4-eba6-4d07-b885-6bf54f935e65', null, '8a89386b-de43-4c63-9127-3a78394d4253', 'Morty', true, 'A goat belonging to Merla Brushgather.', 'Town1');
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
-  VALUES (3, null, 1, 'Orsik Ironfist', true, 'Local blacksmith, favours the drink too much.', 'Town1');
+  VALUES ('a1830913-9213-464c-8139-485ba7238d7c', null, '8a89386b-de43-4c63-9127-3a78394d4253', 'Orsik Ironfist', true, 'Local blacksmith, favours the drink too much.', 'Town1');
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
-  VALUES (4, null, 1, 'Eryn Tharivol', true, 'Owner of Green Dragon Inn.', 'Town1, secretly part of Illuminati');
+  VALUES ('74c7ec40-45d9-46e5-8ab3-cd2f1448fe1d', null, '8a89386b-de43-4c63-9127-3a78394d4253', 'Eryn Tharivol', true, 'Owner of Green Dragon Inn.', 'Town1, secretly part of Illuminati');
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
-  VALUES (5, null, 3, 'Ash Ketchum', true, `Aiming to be the world's best Pokemon trainer!`, 'Pallet Town');
+  VALUES ('6f83b2c1-7a35-431c-b6f7-b8998945c478', null, 'b819024a-4fd2-4316-8697-411ad293bb71', 'Ash Ketchum', true, `Aiming to be the world's best Pokemon trainer!`, 'Pallet Town');
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
-  VALUES (6, null, 3, 'Jesse', true, `Member of Team Rocket.`, 'Viridian City');
+  VALUES ('315cbe8a-8bff-4e02-aa73-adeb16193374', null, 'b819024a-4fd2-4316-8697-411ad293bb71', 'Jesse', true, `Member of Team Rocket.`, 'Viridian City');
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
-  VALUES (7, null, 3, 'James', true, `Member of Team Rocket.`, 'Viridian City');
+  VALUES ('753edd75-7cda-4953-a1fb-f160e7330c7b', null, 'b819024a-4fd2-4316-8697-411ad293bb71', 'James', true, `Member of Team Rocket.`, 'Viridian City');
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
-  VALUES (8, null, 3, 'Meowth', true, `Member of Team Rocket.`, 'Viridian City');
+  VALUES ('c6f18a16-9d2e-4ec7-995c-be066f95b2d7', null, 'b819024a-4fd2-4316-8697-411ad293bb71', 'Meowth', true, `Member of Team Rocket.`, 'Viridian City');
 
