9872244b0fea060005b6f314a716587d0df51aa7
0lgatron <perronolga@gmail.com>
2022-03-18 18:32:58 -0400

    tutorial progress3

--- client/src/tutorial-components/Board.js
+++ client/src/tutorial-components/Board.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import Knight from './Knight'
 import Square from './Square'
+import { canMoveKnight, moveKnight } from './Game'
 
-function renderSquare(i, [knightX, knightY]) {
+function renderSquare(i, knightPosition) {
   const x = i % 8
   const y = Math.floor(i / 8)
-  const isKnightHere = x === knightX && y === knightY
+  const isKnightHere = x === knightPosition[0] && y === knightPosition[1]
   const black = (x + y) % 2 === 1
   const piece = isKnightHere ? <Knight /> : null
 
   return (
-    <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
+    
+    <div onClick={() => handleSquareClick(x, y)} key={i} style={{ width: '12.5%', height: '12.5%' }}>
       <Square black={black}>{piece}</Square>
     </div>
   )
+}
+
+function handleSquareClick(toX, toY) {
+  if (canMoveKnight(toX, toY)) {
+    moveKnight(toX, toY)
+  }
 }
 
 function Board({ knightPosition }) {
