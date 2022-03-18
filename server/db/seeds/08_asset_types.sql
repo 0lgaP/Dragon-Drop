1adc02d87df54e0eee79bf3f4c6339d5dcd3a1e8
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-18 15:27:29 -0400

    add seeds for asset types

---
+++ server/db/seeds/08_asset_types.sql
@@ -0,0 +1,14 @@
+-- 1. 2eb2339a-0adf-4cc4-bc09-6e27d43f15b4
+-- 2. fa8dbb44-f356-45ae-9b57-4c07c95c56f0
+-- 3. ced32085-6daf-464f-b341-daf3624999cf
+-- 4. c11c94ca-e8ef-4539-9b4c-35a4092ba526
+-- 5. cc620ff0-a677-457b-a625-f52f39347bca
+-- 6. 53366308-cb8a-4c69-bdef-8efd546b0d5d
+
+INSERT INTO asset_types (id, name) VALUES
+('2eb2339a-0adf-4cc4-bc09-6e27d43f15b4', 'NPC'),
+('fa8dbb44-f356-45ae-9b57-4c07c95c56f0', 'PLAYER'),
+('ced32085-6daf-464f-b341-daf3624999cf', 'IMAGE');
+-- ('c11c94ca-e8ef-4539-9b4c-35a4092ba526',),
+-- ('cc620ff0-a677-457b-a625-f52f39347bca',),
+-- ('53366308-cb8a-4c69-bdef-8efd546b0d5d',);
