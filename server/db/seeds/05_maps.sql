3cea3ae576a5ecf03b4d61499001fdc3098fb956
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-18 11:52:15 -0400

    add seeds for maps

---
+++ server/db/seeds/05_maps.sql
@@ -0,0 +1,20 @@
+INSERT INTO maps (name, campaign_id) VALUES
+('Tavern', 1),
+('Arcade', 1),
+('Mountain', 1),
+('Dungeon ', 1);
+INSERT INTO maps (name, campaign_id) VALUES
+('Tavern2', 2),
+('Arcade2', 2),
+('Mountain2', 2),
+('Dungeon2 ', 2);
+INSERT INTO maps (name, campaign_id) VALUES
+('Tavern3', 3),
+('Arcade3', 3),
+('Mountain3', 3),
+('Dungeon3 ', 3);
+INSERT INTO maps (name, campaign_id) VALUES
+('Tavern4', 4),
+('Arcade4', 4),
+('Mountain4', 4),
+('Dungeon4 ', 4);
