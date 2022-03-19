1e87efa9a9173b2fc3561f09f3330b7d5f06d461
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-19 20:52:24 -0230

    add content to NPCList

--- client/src/components/campaign/NPCList.jsx
+++ client/src/components/campaign/NPCList.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const NPCList = () => {
+  const list = props.npcs.map((character) => {
 
+    <NPCListItem key={character.id}  name={character.name} bio={character.bio} details={character.details} alive={character.alive} />
+  })
+
+  return (
+    <section className="npc-list">
+      <ul>
+        {list}
+      </ul>
+    </section>
+  )
+  
 }
 
 export default NPCList;
