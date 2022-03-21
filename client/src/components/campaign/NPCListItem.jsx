c8b3ce4872baa6ffc726dd6fd29dc4eeacb8f801
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-21 16:42:59 -0230

    add npcListItem

--- client/src/components/campaign/NPCListItem.jsx
+++ client/src/components/campaign/NPCListItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const NPCListItem = () => {
-
+const NPCListItem = (props) => {
+return (
+  <div>
+    <div className="npc-card--header">
+      <img></img>
+      <h5>{props.name}</h5>
+    </div>
+    <div className="npc-card--content">
+      <p>{props.bio}</p>
+      <p>{props.details}</p>
+    </div>
+  </div>
+)
 }
 
 export default NPCListItem;
