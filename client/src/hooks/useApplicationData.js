6341e4e2dd9b2dac9a8ef1b6c824fb2ef47b5710
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-21 16:57:59 -0230

    adjust applicationData hook

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -15,9 +15,9 @@ function useApplicationData() {
   useEffect(() => {
 // CHANGE TO OUR DATABASE QUERIES:
     Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`)
+      // axios.get(`/api/days`),
+      // axios.get(`/api/appointments`),
+      // axios.get(`/api/interviewers`)
     ]).then(all => {
       setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
     })
