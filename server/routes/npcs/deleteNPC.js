3959edb240fb99efc038099155a6c0d3c3f03f00
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-21 13:35:02 -0230

    add edit/delete to npc and party

---
+++ server/routes/npcs/deleteNPC.js
@@ -0,0 +1,4 @@
+module.exports = (router, db) => {
+  // DELETE NPC
+  router.delete("/users/:id/campaigns/:c_id/npcs/:n_id", (req, res) => {});
+};
---
+++ server/routes/party/editPlayer.js
@@ -0,0 +1,4 @@
+module.exports = (router, db) => {
+  // EDIT PLAYER
+  router.put("/users/:id/campaigns/:c_id/party/:p_id", (req, res) => {});
+};
