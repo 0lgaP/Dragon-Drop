54159084fa83e957574a0387672ac970e4cbbb4a
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-22 13:33:04 -0400

    add comment for testing url

--- client/src/components/campaign/MapDetails.jsx
+++ client/src/components/campaign/MapDetails.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Link, useParams } from 'react-router-dom';
 import Map from "../../components/map";
 
+// Test URL
+// http://localhost:3002/users/2c41cf56-a6d7-11ec-b909-0242ac120002/campaigns/8a89386b-de43-4c63-9127-3a78394d4253/maps/927432f7-7839-4a6d-817f-8e1a925b2706
+// Campaign: Middle Earth
+// Map: Tavern
+// User: rick.sandchez@gmail.com
+  // Pass: pass
 const MapDetails = () => {
   const { u_id, c_id, m_id } = useParams();
 
