d73104bd143401f11db393e9641d63b736b59d2b
0lgatron <perronolga@gmail.com>
2022-03-22 15:02:50 -0400

    process try to add db pull for campain

--- client/src/components/campaign/CampaignList.jsx
+++ client/src/components/campaign/CampaignList.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import {React, useContext} from "react";
+import useFetch from "../../hooks/useFetch";
+import AuthContext from "../../providers/AuthProvider";
 
 const CampaignList = () => {
+  const { auth } = useContext(AuthContext);
+  const userAuth = window.localStorage.getItem("user_id")
+  console.log("USERAUTH", userAuth)
+  const address = `/users/${userAuth}/campaigns` ;
+  const { data: user, error, isPending } = useFetch(`http://localhost:8082${address}`);
+  // console.log("DATA", user)
 
+return(
+  <div>Campain</div>
+)
 }
 
 export default CampaignList;
--- server/routes/campaigns/getCampaigns.js
+++ server/routes/campaigns/getCampaigns.js
@@ -1,4 +1,9 @@
 module.exports = (router, db) => {
   // GET CAMPAIGNS
-  router.get("/users/:id/campaigns", (req, res) => {});
+  router.get("/users/:id/campaigns", (req, res) => {
+    const user = req.session.user
+    console.log(user)
+
+
+  });
 };
