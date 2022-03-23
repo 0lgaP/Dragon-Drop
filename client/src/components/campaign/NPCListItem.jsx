5517b2ad75f1c0c6a8fc64e3f0d7e19f21913ffe
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-23 00:10:27 -0230

    add layout to home

--- client/src/components/campaign/NPCListItem.jsx
+++ client/src/components/campaign/NPCListItem.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const NPCListItem = (props) => {
 return (
-  <div>
-    <div className="npc-card--header">
+  <div className="bg-secondary text-textcolor">
+    <div className="npc-card--header flex flex-column">
       <img></img>
       <h5>{props.name}</h5>
     </div>
-    <div className="npc-card--content">
+    <div className="npc-card--content flex flex-row">
       <p>{props.bio}</p>
       <p>{props.details}</p>
     </div>
