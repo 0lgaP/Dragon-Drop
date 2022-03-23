31c70c940d9e2827f4ce761efba7158947f4563d
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-22 23:13:55 -0230

    add npc routes and db queries

--- server/routes/npcs/deleteNPC.js
+++ server/routes/npcs/deleteNPC.js
@@ -1,4 +1,8 @@
 module.exports = (router, db) => {
   // DELETE NPC
   router.delete("/users/:id/campaigns/:c_id/npcs/:n_id", (req, res) => {});
+db.query(`DELETE FROM npcs WHERE id = $1;`, [req.params.n_id])
+.then(result => {
+// res.send(result)
+})
 };
