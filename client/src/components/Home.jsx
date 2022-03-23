5ec8b4ce75da4812e6626a3dc2804f7487e01701
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-23 12:57:36 -0230

    add styling to home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Home = () => {
 return (
   <section className="flex flex-row justify-evenly m-3">
-    <div className="bg-primary text-textcolor flex flex-column px-20">
+    <div className="bg-primary rounded-xl text-textcolor flex flex-column px-20">
       <h1 className="text-5xl p-10">Maps</h1>
     </div>
-    <div className="bg-primary text-textcolor flex flex-column px-20">
+    <div className="bg-primary rounded-xl text-textcolor flex flex-column px-20">
       <h1 className="text-5xl p-10">Story</h1>
     </div>
-    <div className="bg-primary text-textcolor flex flex-column px-20">
+    <div className="bg-primary rounded-xl text-textcolor flex flex-column px-20">
       <h1 className="text-5xl p-10">Party</h1>
     </div>
-    <div className="bg-primary text-textcolor flex flex-column px-20">
+    <div className="bg-primary rounded-xl text-textcolor flex flex-column px-20">
       <h1 className="text-5xl p-10">NPCs</h1>
     </div>
   </section>
