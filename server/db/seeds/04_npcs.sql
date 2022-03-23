9f5f6e74ea14135e50c112c5f4c2d19106ed6491
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-23 12:59:52 -0400

    fix seeds

--- server/db/seeds/04_npcs.sql
+++ server/db/seeds/04_npcs.sql
@@ -15,5 +15,5 @@ INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details)
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
   VALUES ('753edd75-7cda-4953-a1fb-f160e7330c7b', 'https://www.serebii.net/pokemonmasters/syncpairs/james.png', 'b819024a-4fd2-4316-8697-411ad293bb71', 'James', true, 'Member of Team Rocket.', 'Viridian City');
 INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details) 
-  VALUES ('c6f18a16-9d2e-4ec7-995c-be066f95b2d7', 'https://media2.giphy.com/media/l4FGw9P0TxeyAMBos/giphy.mp4', 'b819024a-4fd2-4316-8697-411ad293bb71', 'Meowth', true, 'Member of Team Rocket.', 'Viridian City');
+  VALUES ('c6f18a16-9d2e-4ec7-995c-be066f95b2d7', 'https://e7.pngegg.com/pngimages/668/856/png-clipart-whiskers-james-meowth-pokemon-pocket-monsters-pokemon-mammal-face.png', 'b819024a-4fd2-4316-8697-411ad293bb71', 'Meowth', true, 'Member of Team Rocket.', 'Viridian City');
 
