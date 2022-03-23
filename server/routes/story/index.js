9bdc1950d7980c8686b87ae0e5a0f3ea36805ae1
0lgatron <61428425+0lgaP@users.noreply.github.com>
2022-03-23 11:12:25 -0400

    Revert "Revert "add editOrder route, fetchStory route, edit getStory route. Amend db …""

--- server/routes/story/index.js
+++ server/routes/story/index.js
@@ -1,16 +1,21 @@
 const postStory = require("./postStory");
-const getStory = require("./getStory");
+const getStories = require("./getStories");
 const editStory = require("./editStory");
 const deleteStory = require("./deleteStory");
+const fetchStory = require("./fetchStory");
 
 module.exports = (router, db) => {
 
   postStory(router, db);
 
-  getStory(router, db);
+  postStoryOrder(router, db);
+
+  getStories(router, db);
 
   editStory(router, db);
 
   deleteStory(router, db);
+
+  fetchStory(router, db);
 
 };
--- server/routes/story/storyCards/deleteStoryCard.js
+++
@@ -1,6 +0,0 @@
-module.exports = (router, db) => {
-  //DELETE Story Card
-  router.delete('/users/:u_id/campaigns/:c_id/story/story_card/:sc_id', (req, res) => {
-
-  })
-};
