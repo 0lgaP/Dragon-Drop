d43939a7d72b763272b031b027e7f6bf1fc08b72
0lgatron <perronolga@gmail.com>
2022-03-23 11:35:01 -0400

    postNpc and Log in merged

--- server/routes/npcs/postNPC.js
+++ server/routes/npcs/postNPC.js
@@ -2,7 +2,7 @@ module.exports = (router, db) => {
   // CREATE NPC
   router.post("/users/:id/campaigns/:c_id/npcs", (req, res) => {});
 
-INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details)
-  VALUES ();
+// INSERT INTO npcs (id, img, campaign_id, name, alive, bio, details)
+//   VALUES ();
 };
 
--- server/routes/users/loginUser.js
+++ server/routes/users/loginUser.js
@@ -10,8 +10,10 @@ module.exports = (router, db) => {
       (result) => {
         if (password === result.rows[0].password) {
           console.log(`result of db query: `, result.rows[0])
+          // req.session["user_id"] = result.rows[0].id
+          // console.log(resul)
           res.send(result.rows[0])
-          req.session["user_id"] = result.rows[0].id
+          
         } else {
           res.send({ error: "error" });
           return;
