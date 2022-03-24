00853bca10dc780f54f6364bae48a80738956009
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-24 06:16:38 -0400

    updated assets

--- server/db/schema/05_create_maps.sql
+++ server/db/schema/05_create_maps.sql
@@ -6,5 +6,6 @@ CREATE TABLE maps (
   id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
   name VARCHAR(100),
   campaign_id UUID REFERENCES campaigns(id) ON DELETE CASCADE,
-  background TEXT DEFAULT 'https://lh3.googleusercontent.com/AJ5H-eUNPd2wAQlJsADGgNNMgG7BTotJG2Uz2hnibIoiSXH_76CKuZKBrJTAaPJoZNRxQ5Q=s0'
+  background TEXT
+  -- 'https://lh3.googleusercontent.com/AJ5H-eUNPd2wAQlJsADGgNNMgG7BTotJG2Uz2hnibIoiSXH_76CKuZKBrJTAaPJoZNRxQ5Q=s0'
 );
