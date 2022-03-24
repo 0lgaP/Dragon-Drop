404f4ea878f964560362ccde652e7ca61d0dce75
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-23 20:31:12 -0400

    add basic link to campaigns maps

--- client/src/components/campaign/MapList.jsx
+++ client/src/components/campaign/MapList.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "../../api/axios";
+import CampContext from "../../providers/CampProvider";
 
 const MapList = () => {
+  const { c_id, u_id } = useParams()
+  const {campaign, setCampaign} = useContext(CampContext)
 
+  const [maps, setMaps] = useState([])
+
+  useEffect(() => {
+    axios.get(`/users/0/campaigns/${campaign}/maps`).then(res => setMaps(res.data))
+  }, [])
+  console.log('hello', maps)
+
+  return (
+    <React.Fragment>
+      {maps.length && maps.map(map => {
+        return <Link to={`maps/${map.id}`}>{ map.name } <img src={ map.background } alt={map.name} /></Link>
+      }) }
+    </React.Fragment>
+  );
 }
 
 export default MapList;
