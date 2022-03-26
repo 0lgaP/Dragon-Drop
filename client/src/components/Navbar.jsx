5f1873700292cbef6cf1618edf35d9e775d9a598
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-26 18:03:10 -0400

    removed map dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,31 +1,33 @@
-import {React, useContext} from "react";
+import { React, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import AuthContext from "../providers/AuthProvider";
 import CampContext from "../providers/CampProvider";
-import './Navbar.css'
+import "./Navbar.css";
 
 const Navbar = (props) => {
   const { auth } = useContext(AuthContext);
   const { campaign_id, campaign } = useContext(CampContext);
-  const userAuth = window.localStorage.getItem("user_id")
-  const u_id = auth.user_id
+  const userAuth = window.localStorage.getItem("user_id");
+  const u_id = auth.user_id;
   // console.log("campaign id status in navbar: ", campaign)
- if (JSON.parse(userAuth) === auth.user_id && campaign()) {
+  if (JSON.parse(userAuth) === auth.user_id && campaign()) {
     return (
       <nav className="bg-primary h-1/16 p-1 ">
         <div className="links flex flex-row justify-evenly h-1/16 m-6 text-textcolor text-xl">
           <div className="dropdown">
-
-            <Link to={ `/users/${u_id}/campaigns/${campaign()}/maps` } className="hover:font-bold flex flex-direction-row items-center">
+            <Link
+              to={`/users/${u_id}/campaigns/${campaign()}/maps`}
+              className="hover:font-bold flex flex-direction-row items-center"
+            >
               Maps
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+              {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
 
                 <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
                 
-              </svg>
+              </svg> */}
             </Link>
 
-            
+            {/*             
             <div class="dropdown-content">
 
               <Link to={ `/users/${u_id}/campaigns/${campaign()}/maps/new` }>
@@ -33,23 +35,36 @@ const Navbar = (props) => {
               </Link>
 
               <a href="#">Delete Map</a>
-            </div>
+            </div> */}
           </div>
-          <Link to={`/users/${u_id}/campaigns/${campaign()}/story`} className="hover:font-bold">Story</Link>
-          <Link to={`/users/${u_id}/campaigns/${campaign()}/party`} className="hover:font-bold">Party</Link>
-          <Link to={`/users/${u_id}/campaigns/${campaign()}/npcs`} className="hover:font-bold">NPCs</Link>
+          <Link
+            to={`/users/${u_id}/campaigns/${campaign()}/story`}
+            className="hover:font-bold"
+          >
+            Story
+          </Link>
+          <Link
+            to={`/users/${u_id}/campaigns/${campaign()}/party`}
+            className="hover:font-bold"
+          >
+            Party
+          </Link>
+          <Link
+            to={`/users/${u_id}/campaigns/${campaign()}/npcs`}
+            className="hover:font-bold"
+          >
+            NPCs
+          </Link>
         </div>
       </nav>
     );
-    }
-  else {
+  } else {
     return (
       <nav className="bg-primary h-1/16 p-4 text-textcolor text-xl ">
         <h2>Get started building YOUR world today!</h2>
       </nav>
-    )
+    );
   }
-
-}
+};
 
 export default Navbar;
