77382a8fff2d7da3d998416e0958fa3d959e8245
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-26 15:23:46 -0400

    add arrow to map dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,8 +16,13 @@ const Navbar = (props) => {
         <div className="links flex flex-row justify-evenly h-1/16 m-6 text-textcolor text-xl">
           <div className="dropdown">
 
-            <Link to={ `/users/${u_id}/campaigns/${campaign()}/maps` } className="hover:font-bold">
+            <Link to={ `/users/${u_id}/campaigns/${campaign()}/maps` } className="hover:font-bold flex flex-direction-row items-center">
               Maps
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                
+              </svg>
             </Link>
 
             
