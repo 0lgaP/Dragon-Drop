e1193e43e1b7c2c1a76e142209c3db6ceca49c03
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-27 11:54:55 -0400

    add basic cards for player sheets

--- client/src/components/campaign/Party/PlayerListItem.jsx
+++ client/src/components/campaign/Party/PlayerListItem.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import NPCCardItem from "../NPCCardItem";
 
 const PlayerListItem = (props) => {
   const { id, user_id, name, sheet_url, profile_pic } = props;
-  
-  return `${JSON.stringify(props)}`;
+
+  return (
+    <NPCCardItem {...props} image={profile_pic}>
+      <div className="text-textcolor">
+        <Link to={{ pathname: sheet_url }} target="_blank">
+          <button className="rounded-md bg-primary p-4 w-[75%]">
+            DnD Beyond Sheet
+          </button>
+        </Link>
+      </div>
+    </NPCCardItem>
+  );
 };
 
 export default PlayerListItem;
