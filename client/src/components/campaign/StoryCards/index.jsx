93f993c072d8463113be98d4af2e5c7e838d11bb
0lgatron <perronolga@gmail.com>
2022-03-27 11:07:59 -0400

    remove storycard pull

--- client/src/components/campaign/StoryCards/index.jsx
+++ client/src/components/campaign/StoryCards/index.jsx
@@ -2,7 +2,6 @@
 import React, {useState, useEffect, useContext} from 'react';
 import Form from "./Form";
 import StoryCardContainer from './StoryCardContainer';
-import StoryCardContainerPull from './StoryCardContainerPull';
 import axios from '../../../api/axios';
 import dataHelper from '../../../hooks/dataHelpers';
 import AuthContext from '../../../providers/AuthProvider';
@@ -25,46 +24,24 @@ export default function StoryCards() {
   const [view, setView] = useState(viewObj.CREATE);
   const [currentStory, setCurrentStory] = useState({});
 
-  //state.maps: id, name, campaign_id, background
-  //state.npc: id, bio, details(mapname) img, campaign_id, name, alive
-  //state.story: campaigns_id, completed, created_on, id, maps_id, npc_id, order_num, story_card_text
-
-  // const [state, setState] = useState({
-  //   npcs: [],
-  //   maps: []
-  // })
-
-
-  // useEffect(() => {
-  //   Promise.all([
-  //     axios.get(`${address}/npcs`),
-  //     axios.get(`${address}/maps`),
-  //   ]).then((all) => {
-  //     setState((prev) => ({
-  //       ...prev,
-  //       npcs: all[0].data,
-  //       maps: all[1].data,
-  //     }));
-  //   });
-  // }, []);
-
+  
   useEffect(() => {
     axios.get(`${address}/story`)
     .then((res) => {
       const storyCardsObject = dataHelper().convertArrayToObject(res.data, 'id')
       setStories(storyCardsObject)
-
+      
     })
   }, [])
-
+  
   const onEdit = (story) => {
     setCurrentStory(story)
     setView(viewObj.EDIT)
     console.log("VIEW", view)
   }
 
-console.log("CURRENT STORY", currentStory)
-
+  console.log("CURRENT STORY", currentStory)
+  
   return(
     <div className="grid-cols-3 flex">
     { view === viewObj.CREATE ? <Form css='card' setStories={setStories}/> :
@@ -86,3 +63,25 @@ console.log("CURRENT STORY", currentStory)
     </div>
   );
 }
+    //state.maps: id, name, campaign_id, background
+    //state.npc: id, bio, details(mapname) img, campaign_id, name, alive
+    //state.story: campaigns_id, completed, created_on, id, maps_id, npc_id, order_num, story_card_text
+  
+    // const [state, setState] = useState({
+    //   npcs: [],
+    //   maps: []
+    // })
+  
+  
+    // useEffect(() => {
+    //   Promise.all([
+    //     axios.get(`${address}/npcs`),
+    //     axios.get(`${address}/maps`),
+    //   ]).then((all) => {
+    //     setState((prev) => ({
+    //       ...prev,
+    //       npcs: all[0].data,
+    //       maps: all[1].data,
+    //     }));
+    //   });
+    // }, []);
