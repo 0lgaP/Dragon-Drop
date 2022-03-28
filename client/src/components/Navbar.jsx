380b85f1cc9345bcc61bcc9e2800e28393d1ffd0
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-28 17:03:12 -0230

    change font in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = (props) => {
   // console.log("campaign id status in navbar: ", campaign)
   if (JSON.parse(userAuth) === auth.user_id && campaign()) {
     return (
-      <nav className="bg-primary h-1/16 p-1 ">
+      <nav className="bg-primary h-1/16 p-1 font-cinzel">
         <div className="links flex flex-row justify-evenly h-1/16 m-6 text-textcolor text-xl">
           <div className="dropdown">
             <Link
@@ -20,22 +20,7 @@ const Navbar = (props) => {
               className="hover:font-bold flex flex-direction-row items-center"
             >
               Maps
-              {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-
-                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-                
-              </svg> */}
             </Link>
-
-            {/*             
-            <div class="dropdown-content">
-
-              <Link to={ `/users/${u_id}/campaigns/${campaign()}/maps/new` }>
-                New Map
-              </Link>
-
-              <a href="#">Delete Map</a>
-            </div> */}
           </div>
           <Link
             to={`/users/${u_id}/campaigns/${campaign()}/story`}
@@ -60,7 +45,7 @@ const Navbar = (props) => {
     );
   } else {
     return (
-      <nav className="bg-primary h-1/16 p-4 text-textcolor text-xl ">
+      <nav className="bg-primary h-1/16 p-4 text-textcolor font-cinzel text-xl ">
         <h2>Get started building YOUR world today!</h2>
       </nav>
     );
