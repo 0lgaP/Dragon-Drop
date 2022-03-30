28d45b1f180bedc8e03a3fb79592dabdbd66b202
Sarah Melanie White <sawrrawr@gmail.com>
2022-03-30 06:42:12 -0230

    add images to home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,19 +10,27 @@ const Home = () => {
       <div className="flex flex-row m-3 justify-evenly">
       <div className="bg-primary rounded-xl text-textcolor flex flex-col tracking-wider p-6 m-4 w-1/4">
         <h1 className="text-4xl px-20 p-10">Maps</h1>
-        <p className="font-inputFont text-lg break-normal text-justify">Our Drag'n'Drop system for map creation allows users to upload a custom image to use as a background. Other images can be loaded in as assets to place on the map in a layering position mechanic. Maps are fully customisable, allowing GMs to tag story cards and NPCs to their maps for better workflow.</p>
+        <p className="font-inputFont text-lg break-normal text-justify">Our Drag'n'Drop system for map creation allows users to upload a custom image to use as a background. </p>
+        <img className="my-4" src="https://i.imgur.com/Uxor8hc_d.jpg?maxwidth=520&shape=thumb&fidelity=high"></img>
+        <p className="font-inputFont text-lg break-normal text-justify">Other images can be loaded in as assets to place on the map in a layering position mechanic. Maps are fully customisable, allowing GMs to tag story cards and NPCs to their maps for better workflow.</p>
       </div>
       <div className="bg-primary rounded-xl text-textcolor flex flex-col tracking-wider p-6 m-4 w-1/4">
         <h1 className="text-4xl px-20 p-10">Story</h1>
-        <p className="font-inputFont text-lg break-normal text-justify">Build your story from the ground up with our storytelling tab. Stories are written in small pieces, and these pieces can be tagged to a specific map or NPC that is part of your campaign. Mark stories as complete when those pieces are used in-game, and they'll move to your archived history for future reference.</p>
+        <p className="font-inputFont text-lg break-normal text-justify">Build your story from the ground up with our storytelling tab. </p>
+        <img className="my-4" src="https://i.imgur.com/MtM9i6H_d.jpg?maxwidth=520&shape=thumb&fidelity=high"></img>
+        <p className="font-inputFont text-lg break-normal text-justify">Stories are written in small pieces, and these pieces can be tagged to a specific map or NPC that is part of your campaign. Mark stories as complete when those pieces are used in-game, and they'll move to your archived history for future reference.</p>
       </div>
       <div className="bg-primary rounded-xl text-textcolor flex flex-col tracking-wider p-6 m-4 w-1/4">
         <h1 className="text-4xl px-20 p-10">Party</h1>
+        <img></img>
         <p className="font-inputFont text-lg break-normal text-justify">Other players on the app can be added as party members. Consider this your player rolodex as each player entry links to their stats sheet, allowing you to check stats and player conditions on the fly.</p>
       </div>
       <div className="bg-primary rounded-xl text-textcolor flex flex-col tracking-wider p-6 m-4 w-1/4">
         <h1 className="text-4xl px-20 p-10">NPCs</h1>
-        <p className="font-inputFont text-lg break-normal text-justify">Create your very own NPC characters in the app to add depth to your adventure. Give them a name, a profile image, a short biography, and you're ready to roll! Each character entry also tracks life status, whether that NPC is still in play or whether they've met their demise.</p>
+        <p className="font-inputFont text-lg break-normal text-justify">Create your very own NPC characters in the app to add depth to your adventure. Give them a name, a profile image, a short biography, and you're ready to roll!
+        </p> 
+        <img className="my-4" src="https://i.imgur.com/2Yry2zc_d.jpg?maxwidth=520&shape=thumb&fidelity=high"></img>
+        <p className="font-inputFont text-lg break-normal text-justify">Each character entry also tracks life status, whether that NPC is still in play or whether they've met their demise.</p>
       </div>
       </div>
     </section>
