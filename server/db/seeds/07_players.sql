df91e3778dec4c951230ce51c6d373f6ffb600a4
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-30 09:36:50 -0400

    update player assets to have profile images

--- server/db/seeds/07_players.sql
+++ server/db/seeds/07_players.sql
@@ -1,13 +1,13 @@
 -- seeds/03_players.sql
 -- players seeds
-INSERT INTO players (id, user_id, campaign_id) VALUES ('db9045ab-5b22-4f67-8699-762817cbcab9', '2c41cf56-a6d7-11ec-b909-0242ac120002', '8a89386b-de43-4c63-9127-3a78394d4253');
-INSERT INTO players (id, user_id, campaign_id) VALUES ('3fb1aa72-e847-4996-be0f-d5f448f82bb0', '4896e484-a6d7-11ec-b909-0242ac120002', '8a89386b-de43-4c63-9127-3a78394d4253');
-INSERT INTO players (id, user_id, campaign_id) VALUES ('56933d01-3d9c-45a4-be3f-db3014061ff5', '62ecd8b6-a6d7-11ec-b909-0242ac120002', '8a89386b-de43-4c63-9127-3a78394d4253');
-INSERT INTO players (id, user_id, campaign_id) VALUES ('28d2bb88-a5a3-49ed-837a-fdff2e2bfab2', '754f982c-a6d7-11ec-b909-0242ac120002', '8a89386b-de43-4c63-9127-3a78394d4253');
-INSERT INTO players (id, user_id, campaign_id) VALUES ('82731626-a197-49ea-9fd0-5c0db319a463', '2c41cf56-a6d7-11ec-b909-0242ac120002', '407a2fd0-f57e-4110-bf73-8f11917ecab9');
-INSERT INTO players (id, user_id, campaign_id) VALUES ('d11cf03e-e17e-4b98-8b66-7781193247fa', '7ee100e2-a6d7-11ec-b909-0242ac120002', '407a2fd0-f57e-4110-bf73-8f11917ecab9');
-INSERT INTO players (id, user_id, campaign_id) VALUES ('e4ec8f3c-a463-4769-a1c8-987083190098', '6bf83ca2-a6d7-11ec-b909-0242ac120002', '407a2fd0-f57e-4110-bf73-8f11917ecab9');
-INSERT INTO players (id, user_id, campaign_id) VALUES ('3b808fff-a0f4-40f3-a729-a87b263eec9e', '6bf83ca2-a6d7-11ec-b909-0242ac120002', 'b819024a-4fd2-4316-8697-411ad293bb71');
-INSERT INTO players (id, user_id, campaign_id) VALUES ('c976d86f-c12b-4279-b617-b3a993438f62', '62ecd8b6-a6d7-11ec-b909-0242ac120002', 'b819024a-4fd2-4316-8697-411ad293bb71');
-INSERT INTO players (id, user_id, campaign_id) VALUES ('4507863c-33eb-4682-9ee5-c955971bd31e', '4896e484-a6d7-11ec-b909-0242ac120002', 'b819024a-4fd2-4316-8697-411ad293bb71');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('db9045ab-5b22-4f67-8699-762817cbcab9', '2c41cf56-a6d7-11ec-b909-0242ac120002', '8a89386b-de43-4c63-9127-3a78394d4253', 'https://cdn.discordapp.com/attachments/954050137271963728/958533271149498428/PurpleBeard.png');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('3fb1aa72-e847-4996-be0f-d5f448f82bb0', '4896e484-a6d7-11ec-b909-0242ac120002', '8a89386b-de43-4c63-9127-3a78394d4253', 'https://cdn.discordapp.com/attachments/954050137271963728/958533271355007056/PinkHAir_lady.png');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('56933d01-3d9c-45a4-be3f-db3014061ff5', '62ecd8b6-a6d7-11ec-b909-0242ac120002', '8a89386b-de43-4c63-9127-3a78394d4253', 'https://cdn.discordapp.com/attachments/954050137271963728/958533493200146462/BlueHairLady.png');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('28d2bb88-a5a3-49ed-837a-fdff2e2bfab2', '754f982c-a6d7-11ec-b909-0242ac120002', '8a89386b-de43-4c63-9127-3a78394d4253', 'https://cdn.discordapp.com/attachments/954050137271963728/958533493430841354/RedBeard_man.png');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('82731626-a197-49ea-9fd0-5c0db319a463', '2c41cf56-a6d7-11ec-b909-0242ac120002', '407a2fd0-f57e-4110-bf73-8f11917ecab9', 'https://cdn.discordapp.com/attachments/954050137271963728/958533271149498428/PurpleBeard.png');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('d11cf03e-e17e-4b98-8b66-7781193247fa', '7ee100e2-a6d7-11ec-b909-0242ac120002', '407a2fd0-f57e-4110-bf73-8f11917ecab9', 'https://cdn.discordapp.com/attachments/954050137271963728/958533271355007056/PinkHAir_lady.png');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('e4ec8f3c-a463-4769-a1c8-987083190098', '6bf83ca2-a6d7-11ec-b909-0242ac120002', '407a2fd0-f57e-4110-bf73-8f11917ecab9', 'https://cdn.discordapp.com/attachments/954050137271963728/958533493200146462/BlueHairLady.png');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('3b808fff-a0f4-40f3-a729-a87b263eec9e', '6bf83ca2-a6d7-11ec-b909-0242ac120002', 'b819024a-4fd2-4316-8697-411ad293bb71', 'https://cdn.discordapp.com/attachments/954050137271963728/958533493430841354/RedBeard_man.png');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('c976d86f-c12b-4279-b617-b3a993438f62', '62ecd8b6-a6d7-11ec-b909-0242ac120002', 'b819024a-4fd2-4316-8697-411ad293bb71', 'https://cdn.discordapp.com/attachments/954050137271963728/958533271149498428/PurpleBeard.png');
+INSERT INTO players (id, user_id, campaign_id, profile_pic) VALUES ('4507863c-33eb-4682-9ee5-c955971bd31e', '4896e484-a6d7-11ec-b909-0242ac120002', 'b819024a-4fd2-4316-8697-411ad293bb71', 'https://cdn.discordapp.com/attachments/954050137271963728/958533271355007056/PinkHAir_lady.png');
 
