cb76cd89b29488f143b9621131269184a30751f5
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-30 10:26:37 -0400

    updated tavern to be custom image

--- server/db/seeds/05_maps.sql
+++ server/db/seeds/05_maps.sql
@@ -22,7 +22,7 @@
 
 -- Campaign 1
 INSERT INTO maps (id, name, campaign_id, background) VALUES
-('927432f7-7839-4a6d-817f-8e1a925b2706','Tavern', '8a89386b-de43-4c63-9127-3a78394d4253', 'https://i.imgur.com/E7mIL3b.jpg'),
+('927432f7-7839-4a6d-817f-8e1a925b2706','Tavern', '8a89386b-de43-4c63-9127-3a78394d4253', 'https://cdn.discordapp.com/attachments/954050137271963728/958533121840660540/Tavern_Background_Only.png'),
 ('27c8b9d0-bbcb-4b64-85d5-3cddb33e3475','Arcade', '8a89386b-de43-4c63-9127-3a78394d4253', 'https://i.imgur.com/BT4y2bR.jpg'),
 ('189a7cb8-1d0a-4a9a-9a24-30661aef861b','Forest', '8a89386b-de43-4c63-9127-3a78394d4253', 'https://lh5.googleusercontent.com/proxy/h-DcqrMqBX0_HlqciUMPRr7GkbKoDAmjsAI3kL9-dOV7LuWucH73h4KQ7qeNZ7nqiHxe9ENkKw=s0'),
 ('2a8de430-62de-4952-a177-f5ca016dc383','Mountain', '8a89386b-de43-4c63-9127-3a78394d4253', 'https://i.redd.it/8ziowf8ov6i31.jpg'),
@@ -30,7 +30,7 @@ INSERT INTO maps (id, name, campaign_id, background) VALUES
 
 -- Campaign 2
 INSERT INTO maps (id, name, campaign_id, background) VALUES
-('70286a70-c3df-4f28-9395-cab13a20e978','Tavern2', '407a2fd0-f57e-4110-bf73-8f11917ecab9', 'https://i.imgur.com/E7mIL3b.jpg'),
+('70286a70-c3df-4f28-9395-cab13a20e978','Tavern2', '407a2fd0-f57e-4110-bf73-8f11917ecab9', 'https://cdn.discordapp.com/attachments/954050137271963728/958533121840660540/Tavern_Background_Only.png'),
 ('a0e1f6ef-1df6-41c6-84c2-35f31f556358','Arcade2', '407a2fd0-f57e-4110-bf73-8f11917ecab9', 'https://i.imgur.com/BT4y2bR.jpg'),
 ('4861db8c-abe2-49f2-9bb3-ddad196fbd73','Forest2', '407a2fd0-f57e-4110-bf73-8f11917ecab9', 'https://lh5.googleusercontent.com/proxy/h-DcqrMqBX0_HlqciUMPRr7GkbKoDAmjsAI3kL9-dOV7LuWucH73h4KQ7qeNZ7nqiHxe9ENkKw=s0'),
 ('0c46957d-2943-4009-bb23-ef3ae2f2f87a','Mountain2', '407a2fd0-f57e-4110-bf73-8f11917ecab9', 'https://i.redd.it/8ziowf8ov6i31.jpg'),
@@ -38,7 +38,7 @@ INSERT INTO maps (id, name, campaign_id, background) VALUES
 
 -- Campaign 3
 INSERT INTO maps (id, name, campaign_id, background) VALUES
-('802a5f86-b0fc-4a9a-95cb-d9a66e494920','Tavern3', 'b819024a-4fd2-4316-8697-411ad293bb71', 'https://i.imgur.com/E7mIL3b.jpg'),
+('802a5f86-b0fc-4a9a-95cb-d9a66e494920','Tavern3', 'b819024a-4fd2-4316-8697-411ad293bb71', 'https://cdn.discordapp.com/attachments/954050137271963728/958533121840660540/Tavern_Background_Only.png'),
 ('f8152357-0ce5-4db8-a7fb-fbd445184436','Arcade3', 'b819024a-4fd2-4316-8697-411ad293bb71', 'https://i.imgur.com/BT4y2bR.jpg'),
 ('47d38653-3c82-43e7-9bd9-3e1d2d5afd26','Forest3', 'b819024a-4fd2-4316-8697-411ad293bb71', 'https://lh5.googleusercontent.com/proxy/h-DcqrMqBX0_HlqciUMPRr7GkbKoDAmjsAI3kL9-dOV7LuWucH73h4KQ7qeNZ7nqiHxe9ENkKw=s0'),
 ('edac5191-6a17-4b85-8af6-4463b25acc1e','Mountain3', 'b819024a-4fd2-4316-8697-411ad293bb71', 'https://i.redd.it/8ziowf8ov6i31.jpg'),
@@ -46,7 +46,7 @@ INSERT INTO maps (id, name, campaign_id, background) VALUES
 
 -- Campaign 4
 INSERT INTO maps (id, name, campaign_id, background) VALUES
-('00972ef3-4927-4fa0-a841-f363cee93648','Tavern4', '06673f43-2b6e-4fd6-812c-0b86d778d5a9', 'https://i.imgur.com/E7mIL3b.jpg'),
+('00972ef3-4927-4fa0-a841-f363cee93648','Tavern4', '06673f43-2b6e-4fd6-812c-0b86d778d5a9', 'https://cdn.discordapp.com/attachments/954050137271963728/958533121840660540/Tavern_Background_Only.png'),
 ('f51aec5b-3379-42ec-807e-24976cbe926b','Arcade4', '06673f43-2b6e-4fd6-812c-0b86d778d5a9', 'https://i.imgur.com/BT4y2bR.jpg'),
 ('1ae3064e-c2f5-43cd-a105-61f573b7eb2c','Forest4', '06673f43-2b6e-4fd6-812c-0b86d778d5a9', 'https://lh5.googleusercontent.com/proxy/h-DcqrMqBX0_HlqciUMPRr7GkbKoDAmjsAI3kL9-dOV7LuWucH73h4KQ7qeNZ7nqiHxe9ENkKw=s0'),
 ('6b545405-9fd6-40e7-8be4-94fab4ba0a7a','Mountain4', '06673f43-2b6e-4fd6-812c-0b86d778d5a9', 'https://i.redd.it/8ziowf8ov6i31.jpg'),
