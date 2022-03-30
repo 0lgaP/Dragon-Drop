95ef1e2cade822fe7b67586bd81dfbc0f3000953
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-03-30 09:34:01 -0400

    update images to custom images

--- server/db/seeds/03_images.sql
+++ server/db/seeds/03_images.sql
@@ -2,12 +2,28 @@
 -- 2. e6f69b18-81cb-4c89-8739-c10ee48a6b92
 -- 3. bb4c3683-7bd2-496c-8a0a-6b7e78e37c1d
 -- 4. fb483912-ae59-4082-9c21-d144443072dc
-
+-- 5. da890eaa-3bda-48de-980c-8c27c2734d55
+-- 6. 5a7bca7d-2c45-4456-89d6-907d8315d6aa
+-- 7. fbd972b8-8161-4ae9-a042-f3517a79e935
+-- 08f4a16d-3c5c-42bf-98fd-d04336657ba8
+-- 4ab58477-9e19-4477-ad8b-3eb0452d2bd3
+-- ef832843-c9f0-4154-a4be-e09f7fb9f096
+-- 15ff6099-1df8-4d08-b642-33478d957a13
+-- 52fdbbde-dc30-4635-a60b-c233ecef8a04
+-- 0f3a6511-5487-4fa4-a47d-77331c2b5a52
+-- c4c9c1af-ac2a-4bf0-9574-2d3925f2bc6c
 INSERT INTO images (id, name, src) VALUES
-('a29a1114-5687-4732-bae6-31e45baa0978','The Cage - Homeless?', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSZjIhQYJAKltl9QjqqSQ4vjk9goWnXTt1zjNaTF4gYzdoGTYOj'),
+('a29a1114-5687-4732-bae6-31e45baa0978','Fireplace', 'https://cdn.discordapp.com/attachments/954050137271963728/958533178010767431/Fireplace.png'),
 
-('e6f69b18-81cb-4c89-8739-c10ee48a6b92','The Cage 2 - Electric Boogaloo', 'https://cdn.britannica.com/64/135864-050-57268027/Nicolas-Cage-2009.jpg'),
+('e6f69b18-81cb-4c89-8739-c10ee48a6b92','Rug', 'https://cdn.discordapp.com/attachments/954050137271963728/958557187549036575/OrbCarpet.png'),
 
-('bb4c3683-7bd2-496c-8a0a-6b7e78e37c1d','The Rock', 'https://www.varietyinsight.com/images/honoree/Dwayne_Johnson.png'),
+('bb4c3683-7bd2-496c-8a0a-6b7e78e37c1d','War Remnents', 'https://cdn.discordapp.com/attachments/954050137271963728/958557187762958376/02_Bood_stain.png'),
 
-('fb483912-ae59-4082-9c21-d144443072dc','Potion Man', 'https://i.kym-cdn.com/photos/images/original/001/801/486/39b.png');
+('fb483912-ae59-4082-9c21-d144443072dc','Potion Man', 'https://i.kym-cdn.com/photos/images/original/001/801/486/39b.png'),
+
+('da890eaa-3bda-48de-980c-8c27c2734d55','Table', 'https://cdn.discordapp.com/attachments/954050137271963728/958557187947528212/Table.png'),
+
+('5a7bca7d-2c45-4456-89d6-907d8315d6aa','Stool', 'https://cdn.discordapp.com/attachments/954050137271963728/958557188199161907/Barel.png'),
+
+('fbd972b8-8161-4ae9-a042-f3517a79e935','Saturday Morning', 'https://cdn.discordapp.com/attachments/954050137271963728/958557188404699156/01_GreenStain.png');
+
